1e9d4b62a4eb04ce9c8ee559ea565ecbdd96e67f
nhd98z <nhd98z@gmail.com>
2024-05-14 02:43:03 +0700

    Init

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { useSelector, useDispatch } from "react-redux"
+import { RootState } from "./app/store"
+import { decrement, increment } from "./slices/counterSlice"
+
+export default function App() {
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <div>
+        <button aria-label="Increment value" onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <span>{count}</span>
+        <button aria-label="Decrement value" onClick={() => dispatch(decrement())}>
+          Decrement
+        </button>
+      </div>
+    </div>
+  )
+}
