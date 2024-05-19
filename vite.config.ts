cd6bd315f4482361d88d6cfad496cb0de02e83b0
nhd98z <nhd98z@gmail.com>
2024-05-20 04:57:04 +0700

    Try pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
+import viteTsconfigPaths from "vite-tsconfig-paths"
+import { nodePolyfills } from "vite-plugin-node-polyfills"
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    nodePolyfills(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Anqa - Best Aggregator On Aptos",
+        short_name: "Anqa",
+        icons: [
+          {
+            src: "favicon.png",
+            sizes: "64x64 32x32 24x24 16x16 192x192 512x512",
+            type: "image/png",
+          },
+        ],
+        start_url: ".",
+        theme_color: "#0079BF",
+        background_color: "#101010",
+        display: "standalone",
+      },
+    }),
+  ],
 })
