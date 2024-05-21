789070c917f200f4b0267f88878f1bb1d0f74f8e
nhd98z <nhd98z@gmail.com>
2024-05-22 02:32:39 +0700

    Integrate petra

--- src/redux/hooks/usePetra.ts
+++ src/redux/hooks/usePetra.ts
@@ -22,7 +22,7 @@ export default function usePetra() {
         //   "redirectLink": "https://anqa-interface.vercel.app",
         //   "dappEncryptionPublicKey": "4d436f77425159444b325675417945417753716355782b515355595846423958377156516a596a767871384b332f6a696436695a66482f584e53513d"
         // }
-        window.open("https://petra.app/api/v1/connect?data=ewogICJhcHBJbmZvIjogewogICAgImRvbWFpbiI6ICJhbnFhLWludGVyZmFjZS52ZXJjZWwuYXBwIgogIH0sCiAgInJlZGlyZWN0TGluayI6ICJodHRwczovL2FucWEtaW50ZXJmYWNlLnZlcmNlbC5hcHAiLAogICJkYXBwRW5jcnlwdGlvblB1YmxpY0tleSI6ICI0ZDQzNmY3NzQyNTE1OTQ0NGIzMjU2NzU0MTc5NDU0MTc3NTM3MTYzNTU3ODJiNTE1MzU1NTk1ODQ2NDIzOTU4Mzc3MTU2NTE2YTU5NmE3Njc4NzEzODRiMzMyZjZhNjk2NDM2Njk1YTY2NDgyZjU4NGU1MzUxM2QiCn0=")
+        window.open("https://petra.app/explore?link=https://anqa-interface.vercel.app")
       }
       return
     }
