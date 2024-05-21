86d0227195a863f94dc2e84ae5fc8298097b0c42
nhd98z <nhd98z@gmail.com>
2024-05-22 02:11:15 +0700

    Integrate petra

--- src/redux/hooks/usePetra.ts
+++ src/redux/hooks/usePetra.ts
@@ -2,15 +2,16 @@ import { useCallback } from "react"
 import { useAppDispatch } from "."
 import { petra } from "../../../global"
 import { connect, disconnect } from "../slices/wallet"
-import { isMobile } from "react-device-detect"
+import { isDesktop } from "react-device-detect"
 
 export default function usePetra() {
   const dispatch = useAppDispatch()
 
   const onConnect = useCallback(async () => {
     if (!petra) {
-      window.open("https://petra.app", "_blank")
-      if (isMobile) {
+      if (isDesktop) {
+        window.open("https://petra.app", "_blank")
+      } else {
         window.open("https://petra.app/api/v1/connect?data=connect_data")
       }
       return
