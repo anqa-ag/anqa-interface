1fa6250b4533a54aaa595b8cb2bf4b9bb1277f62
nhd98z <nhd98z@gmail.com>
2024-05-23 15:10:46 +0700

    Test cache

--- src/App.tsx
+++ src/App.tsx
@@ -241,7 +241,7 @@ export default function App() {
                   isLoading={isSwapping}
                   onPress={connectedWallet ? onDisconnect : onOpenModalConnectWallet}
                 >
-                  <TitleT2>{connectedWallet ? "Disconnect" : `Connect ${process.env.VERCEL_GIT_COMMIT_SHA}`}</TitleT2>
+                  <TitleT2>{connectedWallet ? "Disconnect" : `Connect ${process.env.VITE_VERCEL_GIT_COMMIT_SHA}`}</TitleT2>
                 </Button>
 
                 <Spacer y={4} />
