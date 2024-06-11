36abfd58ec9bf5f02ded03b3e321310d66cca82e
nhd98z <nhd98z@gmail.com>
2024-06-11 18:34:10 +0700

    try purge

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -27,4 +27,4 @@ export const SOURCE_LIST = [
 ]
 
 export const AGGREGATOR_URL = import.meta.env.VITE_AGGREGATOR_URL
-export const VERSION = 2 // Pump version to purge state.
+export const VERSION = 1 // Pump version to purge state.
