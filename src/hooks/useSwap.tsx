077aba15f1efa303207c8964fe1881f2032bd932
nhd98z <nhd98z@gmail.com>
2024-06-18 17:58:53 +0700

    fix: dependencies

--- src/hooks/useSwap.tsx
+++ src/hooks/useSwap.tsx
@@ -418,7 +418,7 @@ export default function useSwap() {
         }
       }
     },
-    [account, connected, isSwapping, sendNotification, signAndSubmitTransaction]
+    [account, connected, isSwapping, sendNotification, signAndSubmitTransaction, signTransaction, telegramUser]
   )
 
   const res = useMemo(
