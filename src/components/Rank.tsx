e3b40bf46870b32a11e5891861143700026850b0
cosi10a1 <anhdt1994@gmail.com>
2024-07-24 10:36:22 +0700

    Update UI badge (#35)
    Co-authored-by: tu.da <anh.dt1@teko.vn>

--- src/components/Rank.tsx
+++ src/components/Rank.tsx
@@ -2,25 +2,33 @@ import { Image } from "@nextui-org/react"
 import { Tooltip as ReactTooltip } from "react-tooltip"
 import { Icon } from "@iconify/react"
 import { useIsSm } from "../hooks/useMedia.ts"
+import { numberWithCommas } from "../utils/number.ts"
+import { Fraction } from "../utils/fraction.ts"
 
-export default function Rank({ image, name, minimumExp }: {
+export default function Rank({ image, name, expLevels, totalPoint }: {
   image: string;
   name: string;
-  minimumExp: number
+  expLevels: Fraction[]
+  totalPoint: Fraction
 }) {
   const isSm = useIsSm()
   return (
-    <div className="flex flex-col items-center rounded-xl bg-[#00123A] p-6 xl:p-4 sm:p-1.5 flex-1">
+    <div className="flex flex-col items-center rounded-xl bg-[#00123A] p-6 xl:p-4 sm:p-1">
       <Image width={isSm ? 48 : 135} src={image} />
-      <Image src={"/images/starrank.png"} />
-      <div className="flex items-center flex-wrap justify-center">
+      <div className="flex gap-2.5 sm:gap-1 mb-0.5">
+        {expLevels.map((value, index) =>
+          (<Image key={`star-${name}-${index}`} width={isSm ? 10 : 21}
+                  src={!totalPoint.lessThan(value) ? "/images/starenable.png" : "/images/stardisable.png"} />)
+        )}
+      </div>
+      <div className="flex items-center flex-wrap justify-center flex-row">
         <div className="mr-1.5 sm:mr-0.5 font-clashDisplay text-base sm:text-[7px] font-bold text-white">{name}</div>
         <Icon width={isSm ? 8 : 18} icon="material-symbols:info-outline" color="#9AA0A6"
-          data-tooltip-id={`tooltip-badge-${name}`} />
+              data-tooltip-id={`tooltip-badge-${name}`} />
       </div>
       <ReactTooltip
         id={`tooltip-badge-${name}`}
-        content={`Minimum ${minimumExp} EXP`}
+        content={`Minimum ${numberWithCommas((expLevels.length > 0 ? expLevels[0] : new Fraction(0)).toSignificant(6), true, 0)} EXP`}
         variant="dark"
         place="right"
         className="!max-w-[min(320px,80%)] !px-2 !py-1"
--- src/components/VolumeBadges.tsx
+++ src/components/VolumeBadges.tsx
@@ -1,19 +1,76 @@
 import Rank from "./Rank.tsx"
+import { Fraction } from "../utils/fraction.ts"
 
-export default function VolumeBadges() {
+interface BadgeConfig {
+  imageUrl: string
+  name: string
+  expLevels: Fraction[]
+}
+
+const badgeConfigs: BadgeConfig[] = [
+  {
+    imageUrl: "/images/plankton.png",
+    name: "Plankton",
+    expLevels: [
+      new Fraction(100),
+      new Fraction(200),
+      new Fraction(300)
+    ]
+  },
+  {
+    imageUrl: "/images/fish.png",
+    name: "Fish",
+    expLevels: [
+      new Fraction(500),
+      new Fraction(700),
+      new Fraction(1000)
+    ]
+  },
+  {
+    imageUrl: "/images/dolphin.png",
+    name: "Dolphin",
+    expLevels: [
+      new Fraction(1500),
+      new Fraction(5000),
+      new Fraction(10000)
+    ]
+  },
+  {
+    imageUrl: "/images/whale.png",
+    name: "Whale",
+    expLevels: [
+      new Fraction(12000),
+      new Fraction(50000),
+      new Fraction(100000)
+    ]
+  },
+  {
+    imageUrl: "/images/megawhale.png",
+    name: "Mega Whale",
+    expLevels: [
+      new Fraction(120000),
+      new Fraction(300000),
+      new Fraction(500000)
+    ]
+  }
+]
+
+export default function VolumeBadges({ totalExp }: { totalExp: Fraction }) {
   return (
     <div
       className="relative grid w-full items-center justify-center rounded-xl sm:rounded-none border-1 border-[#0165A3] bg-[radial-gradient(132.5%_219.17%_at_80.49%_128.61%,#2667FE_0%,#0041DA_9.72%,#030181_21.71%,#001343_36.96%,#000000_69.59%)] px-10 py-8 text-white xl:px-8 xl:py-6 sm:py-2.5 sm:px-2">
       <div className="mb-7 sm:mb-2 flex gap-5 xl:gap-2">
-        <Rank image="/images/plankton.png" name="Plankton" minimumExp={100} />
-        <Rank image="/images/fish.png" name="Fish" minimumExp={500} />
-        <Rank image="/images/dolphin.png" name="Dolphin" minimumExp={1500} />
-        <Rank image="/images/whale.png" name="Whale" minimumExp={12000} />
-        <Rank image="/images/megawhale.png" name="Mega Whale" minimumExp={120000} />
+        {badgeConfigs.map((value) => (
+          <Rank key={`rank-${value.name}`} image={value.imageUrl} name={value.name} totalPoint={totalExp}
+                expLevels={value.expLevels} />
+        ))}
       </div>
       <div className="flex items-center gap-8 sm:gap-6 justify-self-start xl:gap-6 lg:gap-4 ">
-        <div className="font-clashDisplayBold sm:font-clashDisplayMedium sm:text-[12px] text-4xl xl:text-2xl">Volume Badges</div>
-        <div className="max-w-[600px] sm:max-w-[200px] flex-auto font-clashDisplayMedium text-base sm:text-[7px] text-buttonSecondary sm:leading-[8px]">
+        <div className="font-clashDisplayBold sm:font-clashDisplayMedium sm:text-[12px] text-4xl xl:text-2xl">
+          Volume Badges
+        </div>
+        <div
+          className="max-w-[600px] sm:max-w-[200px] flex-auto font-clashDisplayMedium text-base sm:text-[7px] text-buttonSecondary sm:leading-[8px]">
           Swap and unlock exclusive Anqa Badges. Each badge celebrates your trading achievements and enhances your
           profile. Keep swapping to collect them all!
         </div>
