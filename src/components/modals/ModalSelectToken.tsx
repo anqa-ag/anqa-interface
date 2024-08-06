237a068cc861289022d3926d817dd0b90d6001a0
cosi10a1 <anhdt1994@gmail.com>
2024-08-06 23:18:35 +0700

    feat: display stable coin (#37)
    Co-authored-by: tu.da <anh.dt1@teko.vn>

--- src/components/modals/ModalSelectToken.tsx
+++ src/components/modals/ModalSelectToken.tsx
@@ -1,5 +1,7 @@
 import {
+    Avatar,
     Button,
+    Chip,
     Image,
     Input,
     Modal,
@@ -43,6 +45,14 @@ const BANNERS = [
         symbol: "UPTOS",
         logoUrl: "/banners/UPTOS.jpg",
     },
+];
+
+const STABLE_COIN_IDS = [
+    "0x5e156f1207d0ebfa19a9eeff00d62a282278fb8719f4fab3a586a0a2c0fffbea::coin::T",
+    "0xa2eda21a58856fda86451436513b867c97eecb4ba099da5775520e0f7492e852::coin::T",
+    "0x407a220699982ebb514568d007938d2447d33667e4418372ffec1ddb24491b6c::coin::T",
+    "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDT",
+    "0xf22bede237a07e121b56d91a491eb7bcdfd1f5907926a9e58338f964a01b17fa::asset::USDC",
 ];
 
 export interface TokenWithBalance extends Token {
@@ -284,8 +294,18 @@ function ModalSelectToken({
         () => Math.min(680, Math.round(windowHeight / 2 / 68) * 68),
         [windowHeight],
     );
-
     const isSm = useIsSm();
+
+    const stableCoinTokens = useMemo(
+        () =>
+            STABLE_COIN_IDS.map((stableCoinId) => {
+                if (stableCoinId in followingTokenData) {
+                    return followingTokenData[stableCoinId]! as Token;
+                }
+                return null;
+            }).filter((item): item is Token => !!item),
+        [followingTokenData],
+    );
 
     return (
         <>
@@ -372,6 +392,23 @@ function ModalSelectToken({
                             ))}
                         </div>
 
+                        <Spacer y={4} />
+                        <div className="flex flex-wrap gap-2">
+                            {stableCoinTokens.map((token) => (
+                                <Chip
+                                    key={`stable-coin-${token.symbol}`}
+                                    classNames={{
+                                        base: "bg-transparent hover:cursor-pointer border-small border-white/50 rounded-lg",
+                                    }}
+                                    onClick={() => {
+                                        setTokenAndClose(token.symbol);
+                                    }}
+                                    avatar={<Avatar src={token.logoUrl} />}
+                                >
+                                    {token.symbol}
+                                </Chip>
+                            ))}
+                        </div>
                         <Spacer y={4} />
 
                         {renderTokenList && (
